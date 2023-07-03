963854c1c2a6a9c5cdfb13a8a5d0fc79a135b7d6
Igor <semiletov-2003@mail.ru>
2023-07-03 16:57:06 +0300

    Fix banned user comments and auto unban

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -17,28 +17,28 @@ class PageController extends AbstractController
         $timezone = new \DateTimeZone('Europe/Moscow');
         $currentDateTime = new \DateTime();
         $currentDateTime->setTimezone($timezone);
+        $currentDateTime->modify('+3 hours');
 
         /**
          * @var User $current_user
          */
 
         $current_user = $this->getUser();
-        if ($current_user){
-            $roles = $current_user->getRoles()[0];
-            if ($roles == 'ROLE_BANNED'){
-                $banned_until = $current_user->getBannedUntil();
-                if ($currentDateTime > $banned_until) {
-                    $current_user->setBannedUntil(null);
-                    $current_user->setRoles(['ROLE_USER']);
+            if ($current_user){
+                $roles = $current_user->getRoles()[0];
+                if ($roles == 'ROLE_BANNED'){
+                    $banned_until = $current_user->getBannedUntil();
+                    if ($currentDateTime > $banned_until) {
+                        $current_user->setBannedUntil(null);
+                        $current_user->setRoles(['ROLE_USER']);
 
-
-                    $em = $doctrine->getManager();
-                    $em->persist($current_user);
-                    $em->flush();
+                        $em = $doctrine->getManager();
+                        $em->persist($current_user);
+                        $em->flush();
                 }
-
             }
         }
+
 
         $users = $doctrine->getRepository(User::class)->findby(['is_active' => true]);
         return $this->render('page/index.html.twig', [ 'users' =>$users
