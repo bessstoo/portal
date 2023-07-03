a1b005e77f4ddc27d54654a9e1e23350d074a9e2
Igor <semiletov-2003@mail.ru>
2023-07-03 13:04:05 +0300

    Version_fix

--- migrations/Version20230703095905.php
+++ migrations/Version20230703095905.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230630155903 extends AbstractMigration
+final class Version20230703095905 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,22 @@ final class Version20230630155903 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, owner_id INT NOT NULL, post_id INT NOT NULL, content LONGTEXT NOT NULL, creation_date DATETIME NOT NULL, deleted_at TINYINT(1) NOT NULL, INDEX IDX_5F9E962A7E3C61F9 (owner_id), INDEX IDX_5F9E962A4B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, owner_id INT NOT NULL, heading VARCHAR(150) NOT NULL, content LONGTEXT NOT NULL, creation_date DATETIME NOT NULL, deleted_at TINYINT(1) NOT NULL, INDEX IDX_5A8A6C8D7E3C61F9 (owner_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962A7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962A4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
         $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE user ADD banned_until DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962A7E3C61F9');
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962A4B89032C');
         $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D7E3C61F9');
+        $this->addSql('DROP TABLE comments');
         $this->addSql('DROP TABLE post');
+        $this->addSql('ALTER TABLE user DROP banned_until');
     }
 }
